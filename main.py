c0540534fc026d36f15d78b3382305b7b83f2010
sarakyang <co0co174@gmail.com>
2025-08-26 20:19:14 +0900

    프론트 변경

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
-import get_news 
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+import get_news
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
+# Serve static files (like css, js) from the 'static' directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/api/news")
+async def get_news_api():
+    """API endpoint to get the list of news articles."""
     list_1 = get_news.jeju_news()
     list_2 = get_news.jemin_news()
     list_3 = get_news.omai_news()
@@ -13,3 +19,9 @@ async def root():
     list_1.extend(list_3)
     
     return list_1
+
+@app.get("/")
+async def read_index():
+    """Serves the main index.html file."""
+    
+    return FileResponse('static/index.html')
