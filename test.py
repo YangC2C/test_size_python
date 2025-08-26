e808034ad1750b68ae44ac9d3845961a0d663fe3
sarakyang <co0co174@gmail.com>
2025-08-26 17:05:56 +0900

    rss사용 가능한 부분 작업

--- test.py
+++ test.py
@@ -1,2 +1,80 @@
-from bs4 import BeautifulSoup
-import requests as req
+# from bs4 import BeautifulSoup as bs
+import xml.etree.ElementTree as ET
+import requests as req 
+from url.can_use_rss import rss_list as rss
+
+def jeju_news():
+    
+    crad_lsit = []
+    
+    a = rss.jeju
+
+    xl = req.get(a)
+    content = xl.content
+
+    root = ET.fromstring(content)
+    
+    for item in root.findall('./channel/item'):
+        title = item.find('title').text
+        where = '제주일보'
+        link = item.find('link').text
+        
+        crad = {
+            "title" : title,
+            'where' : where,
+            'link' : link
+        }
+        
+        crad_lsit.append(crad)
+        
+    return crad_lsit
+    
+def jemin_news():
+    crad_lsit = []
+    
+    a = rss.jemin
+
+    xl = req.get(a)
+    content = xl.content
+
+    root = ET.fromstring(content)
+    
+    for item in root.findall('./channel/item'):
+        title = item.find('title').text
+        where = '제민일보'
+        link = item.find('link').text
+        
+        crad = {
+            "title" : title,
+            'where' : where,
+            'link' : link
+        }
+        
+        crad_lsit.append(crad)
+        
+    return crad_lsit
+    
+def omai_news():
+    
+    crad_list = []
+    
+    a =rss.omai
+    
+    xl = req.get(a)
+    content = xl.content
+
+    root = ET.fromstring(content)
+    for item in root.findall('./channel/item')[:5]:
+        title = item.find('title').text
+        where = '오마이'
+        link = item.find('link').text
+        
+        crad = {
+            "title" : title,
+            'where' : where,
+            'link' : link
+        }
+        
+        crad_list.append(crad)
+    return crad_list
+    
---
+++ url/can_use_rss.py
@@ -0,0 +1,7 @@
+class rss_list:
+    jemin = 'https://www.jemin.com/rss/S1N15.xml'
+    jeju = 'http://www.jejunews.com/rss/clickTop.xml'
+    omai = 'https://rss.ohmynews.com/rss/top.xml'
+
+#html 파싱
+# 중앙일보, # 조선일보 / 한라일보 / 한겨례 / 동아 / 경향
